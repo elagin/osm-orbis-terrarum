28d6ee1ea897d88e6808c82007ce856d04de0bfc
Pavel Elagin <elagin.pasha@gmail.com>
2014-02-25 15:07:35 +0400

    Добавил в файлы текст лицензии.

--- OrbisTerrarum/Tools.cs
+++ OrbisTerrarum/Tools.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Orbis Terrarum. Export OpenStreetMap tiles to OziExplorer map
- * Copyright © 2014 Pavel Elagin
+ * Copyright © 2014 Pavel Elagin elagin.pasha@gmail.com
 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
